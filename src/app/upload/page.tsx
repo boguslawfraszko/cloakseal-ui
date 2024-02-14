36fd6f5d48fb23fa7ef49040860424ca6e870419
Boguslaw Fraszko <boguslaw.fraszko@gmail.com>
2024-02-14 23:02:49 +0100

    adding FileCards part1

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -3,6 +3,7 @@
 import {FileUploader} from '@/components/FileUploader'
 import {FadeIn} from '@/components/FadeIn';
 import {Container} from '@/components/Container';
+import FileCard from '@/components/FileCard';
 
 export default function Contact() {
   return (
@@ -14,7 +15,7 @@ export default function Contact() {
         </Container>
         <FadeIn className="lg:order-last">
         <div className="grid grid-cols-1 gap-x-8 gap-y-24 lg:grid-cols-2">
-
+            <FileCard/>.
         </div>
       </FadeIn>
     </>
