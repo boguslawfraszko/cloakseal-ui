bdcbbffb4698dd50edad5049653a273f2ed072a3
Boguslaw Fraszko <boguslaw.fraszko@gmail.com>
2024-02-29 23:38:56 +0100

    sorting and filtering part 2 - columns

--- src/app/upload/SearchFilters.tsx
+++ src/app/upload/SearchFilters.tsx
@@ -1,7 +1,10 @@
+'use client'
+
+
 import React, { useState, useEffect } from 'react';
-import { Container } from '@/components/Container';
 import { Disclosure } from '@headlessui/react';
 import { ChevronDownIcon, FunnelIcon } from '@heroicons/react/20/solid';
+import {Container} from '@/components/Container';
 
 export function SearchFilters({ onSearchChange, onSortChange, onStatusFilterChange, onDateRangeChange }) {
 	const [filtersApplied, setFiltersApplied] = useState(0);
@@ -29,83 +32,124 @@ export function SearchFilters({ onSearchChange, onSortChange, onStatusFilterChan
 	};
 
 	return (
-		<Disclosure as="div" className="bg-white">
-			{({ open }) => (
-				<>
-					<div className="border-b border-t border-gray-200 px-4 py-3 sm:px-6 lg:px-8 flex justify-between items-center">
-						<div className="flex items-center">
-							<Disclosure.Button className="group flex items-center text-gray-700">
-								<FunnelIcon className="mr-2 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
-								<span>{filtersApplied} Filters</span>
-								<ChevronDownIcon className={`${open ? 'transform rotate-180' : ''} ml-2 h-5 w-5 text-gray-400`} aria-hidden="true" />
-							</Disclosure.Button>
-							<button type="button" className="ml-4 text-gray-700" onClick={handleClearAll}>
-								Clear all
-							</button>
-						</div>
-						<select
-							value={sortOrder}
-							onChange={(e) => {
-								setSortOrder(e.target.value);
-								onSortChange(e.target.value);
-							}}
-							className="form-select px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-						>
-							<option value="latest">Latest</option>
-							<option value="oldest">Oldest</option>
-						</select>
-					</div>
-					<Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-						<Container className="flex flex-col items-center justify-center bg-transparent">
-							<div className="flex flex-wrap gap-4 mb-4">
-								<input
-									type="text"
-									placeholder="Search..."
-									value={searchTerm}
-									onChange={(e) => {
-										setSearchTerm(e.target.value);
-										onSearchChange(e.target.value);
-									}}
-									className="form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-								/>
-								<select
-									value={statusFilter}
-									onChange={(e) => {
-										setStatusFilter(e.target.value);
-										onStatusFilterChange(e.target.value);
-									}}
-									className="form-select px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-								>
-									<option value="">All Statuses</option>
-									<option value="SENT">SENT</option>
-									<option value="SIGNED">SIGNED</option>
-									<option value="SIGN_PENDING">SIGN_PENDING</option>
-								</select>
-								<input
-									type="date"
-									value={startDate}
-									onChange={(e) => {
-										setStartDate(e.target.value);
-										onDateRangeChange(e.target.value, endDate);
-									}}
-									className="form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-									placeholder="Start Date"
-								/>
-								<input
-									type="date"
-									value={endDate}
-									onChange={(e) => {
-										setEndDate(e.target.value);
-										onDateRangeChange(startDate, e.target.value);
-									}}
-									className="form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-									placeholder="End Date"
-								/>
+		<Container className="mt-8 sm:mt-10 lg:mt-12">
+			<Disclosure
+				as="section"
+				aria-labelledby="filter-heading"
+				className="grid items-center border-b border-t border-gray-200"
+			>
+
+				{({ open }) => (
+					<>
+						<div className="relative col-start-1 row-start-1 py-4">
+							<div
+								className="mx-auto flex max-w-7xl space-x-6 divide-x divide-gray-200 px-4 text-sm sm:px-6 lg:px-8">
+								<div>
+									<Disclosure.Button className="group flex items-center text-gray-700">
+										<FunnelIcon className="mr-2 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+										            aria-hidden="true"/>
+										<span>{filtersApplied} Filters</span>
+										<ChevronDownIcon
+											className={`${open ? 'transform rotate-180' : ''} ml-2 h-5 w-5 text-gray-400`}
+											aria-hidden="true"/>
+									</Disclosure.Button>
+								</div>
+								<div className="pl-6">
+									<button type="button" className="text-gray-500" onClick={handleClearAll}>
+										Clear all
+									</button>
+								</div>
 							</div>
-						</Container>
-					</Disclosure.Panel>
-				</>
-			)}
-		</Disclosure>
+						</div>
+						<Disclosure.Panel className="border-t border-gray-200 py-10">
+							<div
+								className="mx-auto grid max-w-7xl grid-cols-1 gap-x-4 px-4 text-sm sm:px-6 md:gap-x-6 lg:px-8" >
+								<div className="grid auto-rows-min grid-cols-4 gap-y-10 md:grid-cols-4 md:gap-x-6">
+									<fieldset>
+										<legend className="block font-medium">Name contains</legend>
+										<div className="space-y-6 pt-6 sm:space-y-4 sm:pt-4">
+											<input
+												type="text"
+												placeholder="Search..."
+												value={searchTerm}
+												onChange={(e) => {
+													setSearchTerm(e.target.value);
+													onSearchChange(e.target.value);
+												}}
+												className="form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+											/>
+										</div>
+									</fieldset>
+									<fieldset>
+										<legend className="block font-medium">Status</legend>
+										<div className="space-y-6 pt-6 sm:space-y-4 sm:pt-4">
+											<select
+												value={statusFilter}
+												onChange={(e) => {
+													setStatusFilter(e.target.value);
+													onStatusFilterChange(e.target.value);
+												}}
+												className="form-select px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+											>
+												<option value="">All Statuses</option>
+												<option value="SENT">SENT</option>
+												<option value="SIGNED">SIGNED</option>
+												<option value="SIGN_PENDING">SIGN_PENDING</option>
+											</select>
+										</div>
+									</fieldset>
+
+									<fieldset>
+										<legend className="block font-medium">Created </legend>
+										<div className="space-y-6 pt-6 sm:space-y-4 sm:pt-4">
+											<label htmlFor="startDate" className="block text-sm font-medium text-gray-700">from:</label>
+											<input
+												type="date"
+												value={startDate}
+												onChange={(e) => {
+													setStartDate(e.target.value);
+													onDateRangeChange(e.target.value, endDate);
+												}}
+												className="form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+												placeholder="Start Date"
+											/>
+											<label htmlFor="startDate" className="block text-sm font-medium text-gray-700">to:</label>
+											<input
+												type="date"
+												value={endDate}
+												onChange={(e) => {
+													setEndDate(e.target.value);
+													onDateRangeChange(startDate, e.target.value);
+												}}
+												className="form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+												placeholder="End Date"
+											/>
+										</div>
+									</fieldset>
+
+
+								</div>
+							</div>
+						</Disclosure.Panel>
+					</>
+				)}
+			</Disclosure>
+		</Container>
 	);
 }
+
+
+/**
+ *
+ * 							<select
+ * 								value={sortOrder}
+ * 								onChange={(e) => {
+ * 									setSortOrder(e.target.value);
+ * 									onSortChange(e.target.value);
+ * 								}}
+ * 								className="form-select px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+ * 							>
+ * 								<option value="latest">Latest</option>
+ * 								<option value="oldest">Oldest</option>
+ * 							</select>
+ */
